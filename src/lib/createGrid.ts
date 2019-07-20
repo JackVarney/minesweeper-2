a74236caaa93a032aec5824c400bcb970b25a2a0
Jack <jackvarney@live.com>
2019-07-20 20:24:51 +0100

    Altered mine placement

--- src/lib/createGrid.ts
+++ src/lib/createGrid.ts
@@ -3,18 +3,41 @@ import { MinesweeperGridCell } from "../types/MinesweeperGridCell";
 import { revealCell } from "./revealCell";
 
 const GRID_SIZE = 10;
+const MINE_COUNT = 10;
 
 const createRange = () => Array.from({ length: GRID_SIZE });
 
 const createDefaultCell = (x: number, y: number): MinesweeperGridCell => ({
   revealed: false,
-  hasMine: Math.random() > 0.9,
+  hasMine: false,
   hasFlag: false,
   point: {
     x,
     y
   }
 });
+
+const addMinesToGrid = (grid: MinesweeperGrid): void => {
+  function addMines(amountOfMinesToAdd: number) {
+    if (amountOfMinesToAdd === 0) return;
+
+    for (let y = 1; y < GRID_SIZE; y += 1) {
+      for (let x = 1; x < GRID_SIZE; x += 1) {
+        const shouldAddMine = !grid[y][x].hasMine && Math.random() > 0.95;
+
+        if (shouldAddMine) {
+          amountOfMinesToAdd -= 1;
+
+          grid[y][x].hasMine = true;
+        }
+      }
+    }
+
+    return addMines(amountOfMinesToAdd);
+  }
+
+  addMines(MINE_COUNT);
+};
 
 const createGrid = (): MinesweeperGrid =>
   createRange().map((row, y) =>
@@ -34,6 +57,8 @@ const createGridWithNoConflicts = (x: number, y: number): MinesweeperGrid => {
   };
 
   const grid = getValidGrid();
+
+  addMinesToGrid(grid);
 
   // user will have already revealed this tile
   return revealCell(grid[y][x], grid);
